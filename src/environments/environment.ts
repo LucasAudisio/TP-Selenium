ebf02367fd1cf6686ae9c0d2da9eed2616a7fca9
LucasAudisio <82542357+LucasAudisio@users.noreply.github.com>
2023-08-14 17:20:32 -0300

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDYqrn5Njd6bI3mWdO4esYonrNYzNYcrm4",
+  
+    authDomain: "selenium-1472f.firebaseapp.com",
+  
+    projectId: "selenium-1472f",
+  
+    storageBucket: "selenium-1472f.appspot.com",
+  
+    messagingSenderId: "1001504024454",
+  
+    appId: "1:1001504024454:web:f4ff446f8037b38bf36e07"
+  
+  };
+  
